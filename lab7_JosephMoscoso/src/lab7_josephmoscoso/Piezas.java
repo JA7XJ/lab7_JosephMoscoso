d65af2cb494d5a374f5310ee0a8691f44664a1ba
JA7XJ <josephmoscoso70@hotmail.com>
2018-06-15 13:52:55 -0600

    Gui creado

--- lab7_JosephMoscoso/src/lab7_josephmoscoso/Piezas.java
+++ lab7_JosephMoscoso/src/lab7_josephmoscoso/Piezas.java
@@ -9,13 +9,6 @@ package lab7_josephmoscoso;
  *
  * @author joseph
  */
-public class Lab7_JosephMoscoso {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Piezas {
     
 }
